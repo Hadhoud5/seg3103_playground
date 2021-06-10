5fffce3b143c6e7c8baec265d950790df4d0db37
Adhish Maheswaran <amahe023@uottawa.ca>
2021-06-10 13:51:23 -0400

    added more cases to the test, found to be a failed test case

--- Lab04/FizzBuzz/src/fizzbuzz/fizzbuzz_test.java
+++ Lab04/FizzBuzz/src/fizzbuzz/fizzbuzz_test.java
@@ -9,6 +9,11 @@ class fizzbuzz_test {
 	@Test
 	void testfinalResult() {
 		assertEquals("fizz",obj.finalResult(3));
+		assertEquals("buzz",obj.finalResult(10));
+		assertEquals("fizzbuzz",obj.finalResult(15));
+		assertEquals("2",obj.finalResult(2));
+		assertEquals("fizzbuzz",obj.finalResult(90));
+		assertEquals("0",obj.finalResult(0));
 	}
 
 }
