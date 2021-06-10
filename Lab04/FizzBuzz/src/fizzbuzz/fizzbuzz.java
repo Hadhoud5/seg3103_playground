45b51b12afe1ed1ec3e06c083eaccc7c076a17fd
Adhish Maheswaran <amahe023@uottawa.ca>
2021-06-10 13:40:10 -0400

    Made Lab 4 using Fizzbuzz, first failed testcase so starting test timer

---
+++ Lab04/FizzBuzz/src/fizzbuzz/fizzbuzz.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+public class fizzbuzz {
+	public static String finalResult(int n){
+		if(n%3==0 &&n%5==0) {
+			return ("fizzbuzz");
+		}else if(n%3==0){
+			return("fizz");
+		}else if(n%5==0){
+			return("buzz");
+		}
+		return String.valueOf(n);
+	}
+}
---
+++ Lab04/FizzBuzz/src/fizzbuzz/fizzbuzz_test.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class fizzbuzz_test {
+	fizzbuzz obj = new fizzbuzz();
+	@Test
+	void testfinalResult() {
+		assertEquals(3,"fizz");
+	}
+
+}
