416843b12f830fc32da6eccdb9b0577fac4b5a92
Adhish Maheswaran <amahe023@uottawa.ca>
2021-06-10 14:02:42 -0400

    Solved the failed test case, when 0 prints fizzbizz instead of 0. Refactored code in order to solve

--- Lab04/FizzBuzz/src/fizzbuzz/fizzbuzz.java
+++ Lab04/FizzBuzz/src/fizzbuzz/fizzbuzz.java
@@ -2,7 +2,10 @@ package fizzbuzz;
 
 public class fizzbuzz {
 	public static String finalResult(int n){
-		if(n%3==0 && n%5==0) {
+		if(n==0) {
+			return "0";
+		}
+		else if(n%3==0 && n%5==0) {
 			return ("fizzbuzz");
 		}else if(n%3==0){
 			return("fizz");
